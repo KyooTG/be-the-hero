6b94abd0ef3075d80efed9c99be6f28f26ce537e
Alonso Fritz <alonsofritz@hotmail.com>
2020-05-13 01:57:29 -0300

    Validação
    Adicionadas Validações ao Backend para: QUERY, BODY, HEADER, PARAMS

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 const express = require('express'); // Importando o Express
 const cors = require('cors');
+const { errors } = require('celebrate');
 const routes = require('./routes'); // Importando Routes.js
 
 const app = express();
@@ -7,5 +8,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());    //Deve ficar antes de todas as requisições
 app.use(routes);
+app.use(errors());
 
 app.listen(3333); // Mandando a Aplicação "ouvir" a porta 3333
